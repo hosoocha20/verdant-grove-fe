d1e9111faedd1dcbcdd32411c74bb21d04736461
Hosoo Cha <hosoo.cha@gmail.com>
2024-06-17 19:25:53 +1200

    make base /

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   const config = {
-    base: `/verdant-grove-fe/`,
+    base: `/`,
     plugins: [react()],
   };
 
